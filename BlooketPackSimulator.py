9af348b2dac7d7ff3e1e4f84f68f3eed3d41f327
YuvAmazing251 <127151332+YuvAmazing251@users.noreply.github.com>
2023-03-06 16:35:56 -0500

    Added Python File

---
+++ BlooketPackSimulator.py
@@ -0,0 +1,22 @@
+import random
+
+Outback = ['OutUnCommon', 'OutRare', 'OutEpic', 'OutLegendery', 'OutChroma']
+
+IceMonster = ['IceUnCommon', 'IceRare', 'IceEpic', 'IceLegendery', 'IceChroma']
+
+Dino = ['DinoUnCommon', 'DinoRare', 'DinoEpic', 'DinoLegendery']
+
+Aquatic = ['AquaUnCommon', 'AquaRare', 'AquaEpic', 'AquaLegendery']
+
+Safari = ['SafUnCommon', 'SafRare', 'SafEpic', 'SafLegendery', 'SafChroma']
+
+Bot = ['BotUnCommon', 'BotRare', 'BotEpic', 'BotLegendery']
+
+Space = ['SpaceUnCommon', 'SpaceRare', 'SpaceEpic', 'SpaceLegendery', 'SpaceChroma']
+
+Breakfast = ['BreakUnCommon', 'BreakRare', 'BreakEpic', 'BreakLegendery']
+
+Medieval = ['MediUnCommon', 'MediRare', 'MediEpic', 'MediLegendery']
+
+Wonderland = ['WonderUnCommon', 'WonderRare', 'WonderEpic', 'WonderLegendery']
+
